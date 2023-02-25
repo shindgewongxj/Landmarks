16acb87db181fce0e134fac02c92675aa50c1e68
shindgewongxj <shindgewongxj@whatsin.ltd>
2023-02-25 10:53:00 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shindge Wong XJ on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
